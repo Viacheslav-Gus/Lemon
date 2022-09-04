f94b85e63503e66b49082a42ef3e376378309d25
Viacheslav-Gus <viacheslav.gus@outlook.com>
2022-09-04 18:57:33 +0300

    Add accordion

--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-// alert("test ggg");
+/*
 prev = document.querySelector("#b-prev");
 next = document.querySelector("#b-next");
 
@@ -121,3 +121,134 @@ slide9.onclick = function() {
 	slide9.classList.add("active");
 	slideImg.src = slide9.querySelector("img").src;
 }
+
+*/
+
+
+
+/*JQUERY*/
+
+slider = $(".slider");
+console.dir(slider);
+
+$('#b-prev').on('click', function() {
+	let elem = $('.slider .slider-small-img li.active').prev();
+	$('.slider .slider-small-img li.active').removeClass('active');
+
+	if (!elem.length) {
+		elem = $('.slider .slider-small-img li:last-child');
+	}	
+	elem.addClass('active');
+
+	$('.slider .slider-small-img').attr( 'src', elem.find( 'img' ).attr( 'src' ) );
+	
+});
+
+
+$('#b-next').on('click', function() {
+	let elem = $('.slider .slider-small-img li.active').next();
+	$('.slider .slider-small-img li.active').removeClass('active');
+
+	if (!elem.length) {
+		elem = $('.slider .slider-small-img li:last-child');
+	}	
+	elem.addClass('active');
+
+	// $('.slider .slider-small-img').attr( 'src', elem.find( 'img' ).css({'border':'2px solid red'}).attr( 'src' ) );
+	$('.slider .slider-small-img').attr( 'src', elem.find( 'img' ).attr( 'src' ) );
+	
+});
+
+// next.onclick = function() {
+// 	let currentSlide = document.querySelector(".slider .slider-small-img li.active");
+
+// 	// console.dir(currentSlide);
+// 	currentSlide.classList.remove("active");
+// 	let nextSlide =  currentSlide.nextElementSibling;
+// 	if (nextSlide) { 
+// 		nextSlide.classList.add("active");
+// 	} else {
+// 		nextSlide = document.querySelector(".slider .slider-small-img li");
+// 		nextSlide.classList.add("active");
+// 	}
+
+// 	slideImg.src = nextSlide.querySelector("img").src;
+
+// }
+
+/*вешаем обработчик события нажатия на элемент*/
+$('.type_1 .headingElem').on('click', function (){
+    /*записываем в переменную блок с контентом*/
+    let thisContentBlock = $(this).parent().find('.descElem');
+    /*проверяем есть ли у блока с контентом класс active
+      если есть то удаляем, если нет то добавляем */
+    if(thisContentBlock.hasClass('active')) {
+        thisContentBlock.removeClass('active')
+    }
+    else {
+        thisContentBlock.addClass('active')
+    }
+})
+
+/* находим все нужные нам headingElem 
+let allElems = document.querySelectorAll('.type_1 .headingElem');
+// прогоняем из через цикл
+allElems.forEach((elem)=>{
+    // вещаем на каждый элемент обработчик на событие click
+    elem.addEventListener('click', function(){
+        // у нажатого элемента получаем родителя
+        let parentElem = this.parentNode;
+        // находим элемент с контентом
+        let contentBlock = parentElem.querySelector('.descElem')
+        // аналогичная проверка на наличие класса active
+        if(contentBlock.classList.contains("active")) {
+            contentBlock.classList.remove('active');
+        }
+        else {
+            contentBlock.classList.add('active');
+        }
+    })
+})
+*/
+
+/*всё тоже самое что и в первом варианте, кроме...*/
+$('.type_2 .headingElem').on('click', function (){
+    /*удаляем класса active у всех активных элементов*/
+    $('.type_2 .descElem.active').removeClass('active')
+
+    let thisContentBlock = $(this).parent().find('.descElem');
+
+    if(thisContentBlock.hasClass('active')) {
+        thisContentBlock.removeClass('active')
+    }
+    else {
+        thisContentBlock.addClass('active')
+    }
+})
+
+/*всё тоже самое, кроме... JS 
+let allElems = document.querySelectorAll('.type_2 .headingElem');
+
+allElems.forEach((elem)=>{
+    elem.addEventListener('click', function(){
+        //находим все активные элементы
+        let descActive = document.querySelectorAll('.type_3 .descElem.active');
+        //прогоняем через цикл и удаляем класс active
+        descActive.forEach((elem)=>{
+            elem.classList.remove('active');
+        })
+
+        let parentElem = this.parentNode;
+
+        let contentBlock = parentElem.querySelector('.descElem')
+
+        if(contentBlock.classList.contains("active")) {
+            contentBlock.classList.remove('active');
+        }
+        else {
+            contentBlock.classList.add('active');
+        }
+    })
+})
+
+*/
